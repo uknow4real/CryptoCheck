6b460ec8eaa43817d18a0f9a0ae40ea238deee70
Sebastian Chmel <sebastian.chmel@stud.fh-campuswien.ac.at>
2021-03-03 01:13:58 +0100

    Homepage Basic Design+Coingecko REST API (ETH/EUR)

--- CryptoCheck/checker/views.py
+++ CryptoCheck/checker/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 def home_screen_view(request):
     ##print(request.headers())
-    return render(request, "base.html", {})
+    response = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=ethereum')
+    ethereum = response.json()
+    return render(request, "check/home.html", {
+        'img': ethereum[0]['image'],
+        'current_price': ethereum[0]['current_price']
+    })
